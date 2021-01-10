1ecc4af5232c10970138e3738e0c2fcfbc724b3f
shubhT1234 <74590038+shubhT1234@users.noreply.github.com>
2021-01-10 22:26:06 +0530

    Add files via upload

---
+++ Box1.js
@@ -0,0 +1,8 @@
+class Box1 extends BaseClass {
+    constructor(x, y, width, height){
+      super(x,y,width,height);
+      this.image = loadImage("rect2.gif");
+                                                                          }
+  
+  };
+  
